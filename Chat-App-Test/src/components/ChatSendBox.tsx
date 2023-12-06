71467ff56eb2072626a803f4d3742d84be62351b
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-12-06 13:54:52 +0100

    asd

--- Chat-App-Test/src/components/ChatSendBox.tsx
+++ Chat-App-Test/src/components/ChatSendBox.tsx
@@ -3,7 +3,6 @@ export default function ChatSendBox() {
     <>
       <form>
         <label>
-          Your Message: 
           <input type="text" name="name" />
         </label>
         <input type="submit" value="Send" />
